592258af624f2ed1f0cc19ef4299f58f17d0e903
Jonathan Zaharia <jzaharia@ggc.edu>
2025-11-21 17:20:45 -0500

    Update soundboard.js

--- js/soundboard.js
+++ js/soundboard.js
@@ -1,1 +1,7 @@
+const voiceSelect = document.getElementById('voice-select');
 
+function playWinnerSound() {
+  const voice = voiceSelect.value;
+  const audio = new Audio(`assets/sounds/${voice}-winner.mp3`);
+  audio.play();
+}
