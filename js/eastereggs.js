ae63da6284b0b41d126e29484720cca1aa74a4e4
Jonathan Zaharia <jzaharia@ggc.edu>
2025-11-21 17:20:19 -0500

    Update eastereggs.js

--- js/eastereggs.js
+++ js/eastereggs.js
@@ -1,1 +1,10 @@
+let clickCount = 0;
+
+gremlin.addEventListener('click', () => {
+  clickCount++;
+  if(clickCount >= 10){
+    triggerStageEvent('💥 DRAMATIC ERROR: Secret Boot Appears! 💥');
+    clickCount = 0;
+  }
+});
 
